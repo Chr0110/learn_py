337889c277c63c45be6fe6a2857da9dbb90d4a43
Chr0110 <mehdigithub123@gmail.com>
2022-03-25 23:36:20 +0100

    g

--- hey.py
+++ hey.py
@@ -1,6 +1,12 @@
-def foisdeux(i,j):
-    b = i * j
-    print(b)
-foisdeux(100,10)
-
-                                                      
+class fruits:
+    def __init__(f, color, size):
+        f.color = color
+        f.size = size
+    def ktbazbi(f):
+        print("chriw had " + p.color + " wl " + p.size + " tfa7a ya wlad lqhab")
+#print(p.color)
+class khdra(fruits):
+    def __init__(f, color, size):
+        super()__init__(f, color, size)
+p = khdra("red","big")
+p.ktbazbi()
