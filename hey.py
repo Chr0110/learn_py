2536718f42706432af22bbf8ebb7292c68c63e94
Chr0110 <eradi-@student.1337.ma>
2022-03-28 19:25:20 +0000

    ghn

--- hey.py
+++ hey.py
@@ -1,12 +1,14 @@
-class fruits:
-    def __init__(f, color, size):
-        f.color = color
-        f.size = size
-    def ktbazbi(f):
-        print("chriw had " + p.color + " wl " + p.size + " tfa7a ya wlad lqhab")
-#print(p.color)
-class khdra(fruits):
-    def __init__(f, color, size):
-        super()__init__(f, color, size)
-p = khdra("red","big")
-p.ktbazbi()
+class numbers:
+    def __iter__(n):
+        n.number = 1
+        return n
+    def __next__(n):
+        x = n.number
+        n.number += 1
+        return x
+myclass = numbers()
+s = iter(myclass)
+print(next(s))
+print(next(s))
+print(next(s))
+print(next(s))
