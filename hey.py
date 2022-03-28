59a9d78335aa0e81f7f959a5f01750f3ee21b34b
Chr0110 <eradi-@student.1337.ma>
2022-03-28 22:54:09 +0000

    dd

--- hey.py
+++ hey.py
@@ -1,15 +1,10 @@
-class numbers:
-    def __iter__(n):
-        n.number = 1
-        return n
-    def __next__(n):
-        x = n.number
-        n.number += 1
-        return x
-myclass = numbers()
-s = iter(myclass)
-print(next(s))
-print(next(s))
-print(next(s))
-print(next(s))
- 
+# r = "wadie"
+# for x in r:
+#     print(x)
+# x = "wadie"
+k = iter("wadie")
+print(next(k))
+print(next(k))
+print(next(k))
+print(next(k))
+print(next(k))
