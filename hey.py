bc7204189bf03b85948368f40a1e8cc8018f7894
Chr0110 <eradi-@student.1337.ma>
2022-03-29 00:38:38 +0000

    ss

--- hey.py
+++ hey.py
@@ -1,10 +1,9 @@
-# r = "wadie"
-# for x in r:
-#     print(x)
-# x = "wadie"
-k = iter("wadie")
-print(next(k))
-print(next(k))
-print(next(k))
-print(next(k))
-print(next(k))
+from key import student_1337
+from key import resturant_1337
+student1 = student_1337("Mehdi", "p03", 2)
+student2 = student_1337("Wa die", "p05", 1)
+lmakla_had_nhar = resturant_1337("l7ot", "djaj")
+print(student1.name, student1.etage, student1.post)
+print(student2.name, student2.etage, student2.post)
+print(lmakla_had_nhar.lghda)
+print(lmakla_had_nhar.l3cha)
