0bfa687a5bba4068b992898c6b55b619b768279f
Chr0110 <eradi-@student.1337.ma>
2022-03-29 03:33:52 +0000

    dbsd

--- hey.py
+++
@@ -1,9 +0,0 @@
-from key import student_1337
-from key import resturant_1337
-student1 = student_1337("Mehdi", "p03", 2)
-student2 = student_1337("Wa die", "p05", 1)
-lmakla_had_nhar = resturant_1337("l7ot", "djaj")
-print(student1.name, student1.etage, student1.post)
-print(student2.name, student2.etage, student2.post)
-print(lmakla_had_nhar.lghda)
-print(lmakla_had_nhar.l3cha)
--- key.py
+++
@@ -1,9 +0,0 @@
-class student_1337():
-    def __init__(self, name, post, etage):
-        self.name = name
-        self.post = post
-        self.etage = etage
-class resturant_1337():
-    def __init__(chkayn_f, lghda, l3cha):
-        chkayn_f.lghda = lghda
-        chkayn_f.l3cha = l3cha
