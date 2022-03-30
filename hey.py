30a9d1d2959666d57376348b1908050f0a57f197
Chr0110 <eradi-@student.1337.ma>
2022-03-30 22:41:40 +0000

    b

--- hey.py
+++ hey.py
@@ -1,10 +1,2 @@
-try:
-  f = open("demofile.txt")
-  try:
-    f.write("Lorum Ipsum")
-  except:
-    print("Something went wrong when writing to the file")
-  finally:
-    f.close()
-except:
-  print("Something went wrong when opening the file")  
+login = raw_input("enter login : ")
+print("ur login is : " + login)
