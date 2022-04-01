03dd7cd195f8b4009cb2aa5f1fc7a2bba52d43f5
Chr0110 <eradi-@student.1337.ma>
2022-04-01 03:12:39 +0000

    gg

--- hey.py
+++ hey.py
@@ -3,25 +3,30 @@ from unittest import result
 import requests
 from bs4 import BeautifulSoup
 import csv
+import re
 from itertools import zip_longest
 pricess_tab = []
 description_tab = []
 category_tab = []
-localisation_tab = []
+time_localisation_tab = []
 result = requests.get("https://www.avito.ma/fr/maroc/%C3%A0_vendre")
 src = result.content
 soup = BeautifulSoup(src, "lxml")
 prices = soup.find_all("div", {"class":"oan6tk-5"})
 description = soup.find_all("span", {"class":"oan6tk-17"})
 catego = soup.find_all("p", {"class":"sc-1x0vz2r-0 eJWySg oan6tk-18 dPesmp"})
-localisation = soup.find_all("span", {"class":"sc-1x0vz2r-0 kIeipZ"})
+time_localisation = soup.find_all("span", {"class":"sc-1x0vz2r-0 kIeipZ"})
 for i in range(len(description)):
     pricess_tab.append(prices[i].text)
     description_tab.append(description[i].text)
     category_tab.append(catego[i].text)
-    if localisation[i].text != int:
-        print(localisation[i].text)
+    time_localisation_tab.append(time_localisation[i].text)
 # print(pricess_tab)
 # print(description_tab)
 # print(category_tab)
-# print(localisation_tab)
+# print(time_localisation_tab)
+first_file = [category_tab, description_tab, pricess_tab, time_localisation_tab]
+exported = zip_longest(*first_file)
+with open("/Users/eradi-/Desktop/learn_py/first.csv", "w") as file:
+    wr = csv.writer(file)
+    wr.writerow(exported)
