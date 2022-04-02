b1b33a14c416936cc35639d8a6ea12ed4265fcbb
Chr0110 <eradi-@student.1337.ma>
2022-04-02 03:27:03 +0000

    c

--- hey.py
+++ hey.py
@@ -29,8 +29,7 @@ catego = soup.find_all("p", {"class":"sc-1x0vz2r-0 eJWySg oan6tk-18 dPesmp"})
 localisation = soup.find_all("span", {"class":"sc-1x0vz2r-0 kIeipZ"})
 time = soup.find_all("span", {"class":"sc-1x0vz2r-0 kIeipZ"})
 links = soup.find_all("div",{"class":"oan6tk-0 hEwuhz"})
-
-
+a = 0
 for i in range(len(description)):
     pricess_tab.append(prices[i].text)
     description_tab.append(description[i].text)
@@ -44,21 +43,23 @@ for j in range(len(links)):
     links_tab.append(links[j].find("a").attrs['href'])
 for link in links_tab:
     result = requests.get(link)
-    src1 = result.content
-    soup1 = BeautifulSoup(src1, "lxml")
-    phone = soup.find_all("h3",{"class":"sc-1x0vz2r-0 kSIxrj"})
+    src = result.content
+    soup1 = BeautifulSoup(src, "lxml")
+    phone = soup1.find_all("h3", {"class":"sc-1x0vz2r-0 kSIxrj"}).text
     print(phone)
-    #phone_tab.append(phone.text)
-   # phone_tab.append(phone.find("a").attrs['href'])
-    #print(links1[a].find("a".attrs['href']))
-
+    #phone2 = soup1.find("a").attrs['href']
+    #a += 1
+    #print(a)
+    #phone_tab.append(phone[a].text)
+    #print(phone2)
+#print(a)
 # print(pricess_tab)
 # print(description_tab)
 # print(category_tab)
 # print(localisation_tab)
-# first_file = [category_tab, description_tab, pricess_tab, localisation_tab, time_tab, links_tab]
-# exported = zip_longest(*first_file)
-# with open("/Users/eradi-/Desktop/learn_py/first.csv", "w") as file:
-#     wr = csv.writer(file)
-#     wr.writerow(["Category", "Description", "Price", "Location", "Time", "Offer link"])
-#     wr.writerows(exported)
+first_file = [category_tab, description_tab, pricess_tab, localisation_tab, time_tab, links_tab, phone_tab]
+exported = zip_longest(*first_file)
+with open("/Users/eradi-/Desktop/learn_py/first.csv", "w") as file:
+    wr = csv.writer(file)
+    wr.writerow(["Category", "Description", "Price", "Location", "Time", "Offer link", "Phone"])
+    wr.writerows(exported)
