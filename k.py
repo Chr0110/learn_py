71030e76f210f8461553759a522b592657b009ef
Chr0110 <eradi-@student.1337.ma>
2022-04-05 07:28:19 +0000

    xx

--- k.py
+++ k.py
@@ -6,8 +6,10 @@ from time import sleep
 
 d=Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=d)
-#driver.minimize_window()
 driver.set_window_size(1000,1000)
 driver.get('https://github.com/Chr0110')
 while 1:
     sleep(3)
+
+    # but = driver.find_element_by_class_name('avatar avatar-user width-full border color-bg-default')
+    # but.click()
