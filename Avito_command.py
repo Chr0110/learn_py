2fb9a4baf8d85eeffba2d4193f57774177e4bacf
Chr0110 <eradi-@student.1337.ma>
2022-04-10 16:02:37 +0000

    h

--- Avito_command.py
+++ Avito_command.py
@@ -20,11 +20,10 @@ src = result.content
 soup = BeautifulSoup(src, "lxml")
 prices = soup.find_all("div", {"class":"oan6tk-5"})
 description = soup.find_all("span", {"class":"oan6tk-17"})
-catego = soup.find_all("p", {"class":"sc-1x0vz2r-0 eJWySg oan6tk-18 dPesmp"})
-localisation = soup.find_all("span", {"class":"sc-1x0vz2r-0 kIeipZ"})
-time = soup.find_all("span", {"class":"sc-1x0vz2r-0 kIeipZ"})
-links = soup.find_all("div",{"class":"oan6tk-0 hEwuhz"})
-a = 0
+catego = soup.find_all("p", {"class":"sc-1x0vz2r-0 iEJWiq oan6tk-18 bhnuSP"})
+localisation = soup.find_all("span", {"class":"sc-1x0vz2r-0 hCOOjL"})
+time = soup.find_all("span", {"class":"sc-1x0vz2r-0 hCOOjL"})
+links = soup.find_all("div",{"class":"oan6tk-0 gbnYeC", "class":"oan6tk-0 dLOfLV"})
 for i in range(len(description)):
     pricess_tab.append(prices[i].text)
     description_tab.append(description[i].text)
@@ -34,14 +33,13 @@ for i in range(len(localisation)):
         localisation_tab.append(localisation[i].text)
     elif time[i].text.find(":") != -1:
         time_tab.append(time[i].text)
-for j in range(len(links)):
-    links_tab.append(links[j].find("a").attrs['href'])
-for link in links_tab:
-    result = requests.get(link)
-    src = result.content  
-    soup = BeautifulSoup(src, "lxml")
-    phone = soup.find("h1", {"class":"sc-1x0vz2r-0 iLDWht"})
-    print(phone.text)
+
+# for link in links_tab:
+#     result = requests.get(link)
+#     src = result.content  
+#     soup = BeautifulSoup(src, "lxml")
+#     phone = soup.find("h1", {"class":"sc-1x0vz2r-0 iLDWht"})
+#     print(phone.text)
 
 first_file = [category_tab, description_tab, pricess_tab, localisation_tab, time_tab, links_tab, phone_tab]
 exported = zip_longest(*first_file)
